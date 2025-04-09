a6beef4a7292bdfafd72e9ac3167cd9059415b29
Pooja Singh <94850256+PoojaSingh-a@users.noreply.github.com>
2025-04-09 12:59:26 +0530

    payment reminders

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,27 +1,38 @@
 const { Resend } = require('resend');
 const resend = new Resend('re_TANhsRc5_F3Y6hyxyxMGTHRvieuqnEMHz'); // Replace with actual API key
 
-async function sendEmail(senderEmail, recipientEmail, clientName, pdfBase64) {
+async function sendEmail(senderEmail, recipientEmail, clientName, pdfBase64,calendarLink) {
     try {
         if (!pdfBase64) {
             throw new Error("Invalid PDF base64 provided");
         }
-
         // Send email using Resend API
         const response = await resend.emails.send({
             from: senderEmail,
             to: recipientEmail,
             subject: 'Your Invoice',
-            text: `This is an invoice for ${clientName}. Please find the attached PDF.`,
+            html: `
+                <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333;"> 
+                    <p>Hello ${clientName}</p>
+                    <p>Please find your invoice attached.</p>
+                    <p>
+                        <a href="${calendarLink}" target="_blank"
+                        style="display: inline-block; padding:12px 24px; margin-top:10px; background-color:#007bff; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">
+                        Add Reminder to Google Calendar
+                        </a>
+                    </p>
+                    <p>Thankyou!</p>
+                </div>
+            ` ,      
             attachments: [
                 {
                     filename: "invoice.pdf",
                     content: pdfBase64,
                     encoding: "base64",
-                },
+                },                
             ],
         });
-        console.log("Email sent successfully.", response);
+       // console.log("Email sent successfully.", response);
     } catch (error) {
         console.error("Error sending email:", error);
         throw error;
